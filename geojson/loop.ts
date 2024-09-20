cb6c8eed284a67272bee36dacc039506cc515dc7
Peter Johnson <insomnia@rcpt.at>
2024-09-20 12:52:48 +0200

    fix(geojson): detect invalid polygon loops

--- geojson/loop.ts
+++ geojson/loop.ts
@@ -20,7 +20,30 @@ export const marshal = (loop: Loop, ordinal: number): geojson.Position[] => {
 export const unmarshal = (ring: geojson.Position[], ordinal: number): Loop => {
   ring = ring.slice() // make a copy to avoid mutating input
   ring.length -= 1 // remove matching start/end points
-  ring = ring.filter((p, i) => !i || !position.equal(ring.at(i - 1)!, p, 0)) // remove equal+adjacent vertices
   if (ordinal > 0) ring.reverse() // ensure all rings are CCW
+
+  // Loops are not allowed to have any duplicate vertices (whether adjacent or not)
+  if (containsDuplicateVertices(ring)) {
+    // adjacent duplicates are fixable
+    ring = removeAdjacentDuplicateVertices(ring)
+
+    // non-adjacent duplicates are not fixable
+    if (containsDuplicateVertices(ring)) return new Loop([])
+  }
+
   return new Loop(ring.map(position.unmarshal))
+}
+
+/**
+ * Removes *adjacent* duplicate (and near-duplicate) vertices from ring.
+ */
+export const removeAdjacentDuplicateVertices = (ring: geojson.Position[], epsilon = 1e-8): geojson.Position[] => {
+  return ring.filter((p, i) => !i || !position.equal(ring.at(i - 1)!, p, epsilon))
+}
+
+/**
+ * Returns true IFF ring contains duplicate vertices at any position.
+ */
+export const containsDuplicateVertices = (ring: geojson.Position[]): boolean => {
+  return ring.some((A, i) => ring.slice(i + 1).some((B) => position.equal(A, B)))
 }
